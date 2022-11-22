13433b8610c5e76b21466c606d6853762946db66
Wallisson Lopes Guevara <wallissonguevara122@outlook.com>
2022-11-22 16:13:56 -0300

    Iniciando Projeto LanchesWeb

---
+++ LanchesWeb/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using LanchesWeb.Models;
+using Microsoft.EntityFrameworkCore;
+namespace LanchesWeb.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Lanche> Lanches { get; set; }
+    }
+}
